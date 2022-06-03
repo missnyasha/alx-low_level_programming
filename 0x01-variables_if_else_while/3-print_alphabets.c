653c17922a8d8c956eff173a23f7e07c0916458d
missnyasha <53005347+missnyasha@users.noreply.github.com>
2022-06-03 22:10:40 +0200

    three

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main 
+ * A program that prints the alphabet in lowercase, and then in uppercase, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+	return (0);
+}
