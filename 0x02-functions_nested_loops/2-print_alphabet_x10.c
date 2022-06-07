678d4be0bf55062e5bead137a01a372a850f398d
missnyasha <53005347+missnyasha@users.noreply.github.com>
2022-06-07 20:01:43 +0200

    two

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * a function that prints 10 times the alphabet, in lowercase, followed by a new line
+ * Return:void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+
+}
